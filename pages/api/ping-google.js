64a4dfda0610b6183df5cfa31d1e6194a53fe891
Muawiya Sadiq <msadiqblog@gmail.com>
2026-01-08 00:40:40 +0100

    Google-ping

--- pages/api/ping-google.js
+++ pages/api/ping-google.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
-  const sitemapUrl = 'https://pmcompare.com/sitemap.xml';
+  const sitemapUrls = [
+    'https://pmcompare.com/sitemap-0.xml'
+    // add sitemap-1.xml, sitemap-2.xml if you have multiple
+  ];
 
   try {
-    await axios.get(`https://www.google.com/ping?sitemap=${sitemapUrl}`);
-    res.status(200).json({ message: 'Ping sent to Google' });
+    for (const url of sitemapUrls) {
+      await axios.get(`https://www.google.com/ping?sitemap=${url}`);
+    }
+    res.status(200).json({ message: 'Ping sent to Google for all sitemaps' });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
