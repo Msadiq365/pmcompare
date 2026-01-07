a4aca98098cf84fdd801777f291c1bbd5006eec4
Muawiya Sadiq <msadiqblog@gmail.com>
2026-01-08 00:26:39 +0100

    Google-ping

--- pages/api/ping-google.js
+++ pages/api/ping-google.js
@@ -1,4 +1,3 @@
-// pages/api/ping-google.js
 import axios from 'axios';
 
 export default async function handler(req, res) {
@@ -6,10 +5,8 @@ export default async function handler(req, res) {
 
   try {
     await axios.get(`https://www.google.com/ping?sitemap=${sitemapUrl}`);
-    console.log(`✅ Successfully pinged Google: ${sitemapUrl}`);
     res.status(200).json({ message: 'Ping sent to Google' });
   } catch (err) {
-    console.error('❌ Error pinging Google:', err.message);
     res.status(500).json({ error: err.message });
   }
 }
